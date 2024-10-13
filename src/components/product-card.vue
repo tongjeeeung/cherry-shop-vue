7522484b197c2ccf4e8b3aba553cef28f66e00d5
tongjeeeung <tongjeeeung@gmail.com>
2024-10-14 01:03:22 +0300

    update all links

--- src/components/product-card.vue
+++ src/components/product-card.vue
@@ -13,7 +13,7 @@ let mouseOver = ref(false)
   >
     <img
       class="h-60 w-52 object-cover"
-      :src="'/cherry-shop-vue' + mouseOver && item.imgSecond !== '' ? item.imgSecond : item.imgFirst"
+      :src="'/cherry-shop-vue' + (mouseOver && item.imgSecond !== '' ? item.imgSecond : item.imgFirst)"
     />
     <div
       v-if="item.sale !== ''"
