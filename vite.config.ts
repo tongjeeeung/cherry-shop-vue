d8bd27f67826bd6f7b46ba50b00feccef962d31e
tongjeeeung <tongjeeeung@gmail.com>
2024-10-14 00:38:35 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), ghPages({
+    branch: 'for-gh-pages'
+  })],
   base: '/cherry-shop-vue/',
   resolve: {
     alias: {
